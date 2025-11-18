2d6d33fa3b783ed15c161613d91699625ae54577
Gaby099x <grabianjugadores@gmail.com>
2025-11-18 18:11:14 -0300

    Delete Backend/index.php

--- Backend/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json; charset=utf-8');
-
-// Mensaje de prueba para saber que la API está activa
-echo json_encode([
-    "ok" => true,
-    "message" => "API funcionando correctamente"
-]);
-?>
